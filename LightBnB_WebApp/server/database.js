fb3e5ec20694b76133e90353e7a34ba78082814a
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-12-12 13:48:42 -0500

    Refactor function to dispay user reservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -76,7 +76,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT reservations.*, properties.*
+    FROM reservations
+    JOIN properties ON properties.id = reservations.property_id
+    WHERE guest_id = $1
+    LIMIT $2;`,
+    [guest_id, limit])
+    .then((reservations) => {
+      return Promise.resolve(reservations.rows);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.getAllReservations = getAllReservations;
 
